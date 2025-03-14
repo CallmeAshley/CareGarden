67335b403370e4f3d643cff34df34548f2a6b397
LSH <4732nuyhus@gmail.com>
2025-03-14 14:41:39 +0900

    Final

--- voice/services/gpt_service.py
+++ voice/services/gpt_service.py
@@ -8,10 +8,11 @@ def correct_text(input_text):
     response = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
-            {"role": "system", "content": "너는 한국어 텍스트 교정 및 중국어 통역 전문가야. "
-                                          "한국인 환자와 중국인 간병인 사이 환자의 발화를 맞춤법과 문맥에 맞게 교정하고, 반말을 존댓말로 공손하게 변경해."
-                                          "만약 텍스트에 '중국어'라는 단어가 마지막에 포함되어 있으면 중국어로 교정된 발화만 제공해."},
-            {"role": "user", "content": f"다음 문장을 교정해: {input_text}"},
+            {"role": "system", "content": "You are an expert in Korean text correction and Chinese interpretation. " \
+                                          "Correct the speech of a Korean patient in terms of spelling and context, " \
+                                          "and change informal language into polite formal language for communication between a Korean patient and a Chinese caregiver. " \
+                                          "If the text ends with the word 'Chinese', only provide the corrected speech in Chinese."},
+            {"role": "user", "content": f"Correct the following sentence.: {input_text}"},
         ],
         temperature=0.2
     )
